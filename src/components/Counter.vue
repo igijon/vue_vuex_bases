7f2107dfede03ea8406ddda9be7590e361d28821
Inma Gijón <igijoninf@gmail.com>
2022-03-01 08:28:11 +0100

    Propiedad computada accediendo al state reactivo

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,20 @@
 <template>
   <h1>Counter - Vuex</h1>
   <h2>Direct access: {{ $store.state.count }}</h2>
+  <h2>Computed: {{ countComputed }}</h2>
 </template>
 
 <script>
 export default {
 
+    /**Los componentes pueden tener sus estados independientes, 
+     * no todo tiene que estar dentro del state global
+     */
+    computed: {
+        countComputed() {
+            return this.$store.state.count
+        }
+    }
 }
 </script>
 
